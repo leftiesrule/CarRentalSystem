f8b2211cbb4a10ea44251c94e9846f819c6ba84b
Ammar Yasar <leftiesrule26627@gmail.com>
2023-07-05 11:28:33 -0700

    fixed error

--- src/App.java
+++ src/App.java
@@ -6,12 +6,11 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to IS Car Rental System!");
         System.out.print("Pleas Enter Your Name: ");
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         String name = scanner.next();
         System.out.println(name);
         System.out.print("Enter Your Password: ");
         String password = scanner.next();
         System.out.println(password);
-
     }
 }
