8159c3a03a790b7fa6e77fd4b425da27afda2f8c
Ammar Yasar <70936998+leftiesrule@users.noreply.github.com>
2023-07-05 11:32:57 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-
+    
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to IS Car Rental System!");
         System.out.print("Pleas Enter Your Name: ");
