9de07a1604ae4cec6999c7fe6a6e69b8077bf04c
msaleh-12 <shai47785@gmail.com>
2023-07-13 16:09:19 +0500

    Project part of Saleh started!

--- src/App.java
+++ src/App.java
@@ -1,18 +1,30 @@
 
 
 import java.util.Scanner;
+
 public class App {
-    
-    public static void main(String[] args) throws Exception {
-    
+
+    public static void NewRegistration(int choice) {
+        if (choice == 1) {
+            // Walk-in Rental
+            System.out.println("Walk-in Rental Entered");
+
+        } else if (choice == 2) {
+            // Future Reservation
+            System.out.println("Future-Reservation Rental Entered");
+
+        } else {
+            System.out.println("Wrong Input! Please input the choice correctly (1 for Walk-in Rental OR 2 for Future Reservation)");
+            return;
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to IS Car Rental System!");
-        System.out.print("Pleas Enter Your Name: ");
-        Scanner scanner1 = new Scanner(System.in);
-        String name = scanner.next();
-        System.out.println(name);
-        System.out.print("Enter Your Password: ");
-        String password = scanner.next();
-        System.out.println(password);
+
+        System.out.print("Please enter your choice of reservation: 1 for Walk-in Rental OR 2 for Future Reservation: ");
+        int choice = scanner.nextInt();
+        NewRegistration(choice);
     }
 }
